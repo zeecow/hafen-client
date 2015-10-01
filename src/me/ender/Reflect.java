69ffb748d2deb3918701a003831befd9ffd75a76
Ender Wiggin <andrii.gook@gmail.com>
2015-10-01 12:25:16 +0300

    improved reflection helper a bit

--- src/me/ender/Reflect.java
+++ src/me/ender/Reflect.java
@@ -3,37 +3,37 @@ package me.ender;
 import java.lang.reflect.Field;
 
 public class Reflect {
-    public static Object getFieldValue(Object obj, String name){
+    public static Object getFieldValue(Object obj, String name) {
 	Object v = null;
 	try {
 	    Field f = getField(obj, name);
 	    v = f.get(obj);
-	} catch (NoSuchFieldException ignored) {
-	} catch (IllegalAccessException ignored) {}
+	} catch (NoSuchFieldException | IllegalAccessException ignored) {
+	}
 	return v;
     }
 
-    public static int getFieldValueInt(Object obj, String name){
+    public static int getFieldValueInt(Object obj, String name) {
 	int v = 0;
 	try {
 	    Field f = getField(obj, name);
 	    v = f.getInt(obj);
-	} catch (NoSuchFieldException ignored) {
-	} catch (IllegalAccessException ignored) {}
+	} catch (NoSuchFieldException | IllegalAccessException ignored) {
+	}
 	return v;
     }
 
-    public static double getFieldValueDouble(Object obj, String name){
+    public static double getFieldValueDouble(Object obj, String name) {
 	double v = 0;
 	try {
 	    Field f = getField(obj, name);
 	    v = f.getDouble(obj);
-	} catch (NoSuchFieldException ignored) {
-	} catch (IllegalAccessException ignored) {}
+	} catch (NoSuchFieldException | IllegalAccessException ignored) {
+	}
 	return v;
     }
 
-    public static String getFieldValueString(Object obj, String name){
+    public static String getFieldValueString(Object obj, String name) {
 	return (String) getFieldValue(obj, name);
     }
 
@@ -41,5 +41,14 @@ public class Reflect {
 	Field f = obj.getClass().getDeclaredField(name);
 	f.setAccessible(true);
 	return f;
+    }
+
+    public static boolean hasField(Object obj, String name) {
+	try {
+	    getField(obj, name);
+	    return true;
+	} catch (NoSuchFieldException ignored) {
+	}
+	return false;
     }
 }
