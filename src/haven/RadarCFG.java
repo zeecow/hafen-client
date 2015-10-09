1f61eff779e94bb952581bd8c2221fe8eeac134c
Ender Wiggin <andrii.gook@gmail.com>
2015-10-09 17:43:40 +0300

    some work on moving radar config to xml

--- src/haven/RadarCFG.java
+++ src/haven/RadarCFG.java
@@ -8,6 +8,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,27 +17,137 @@ public class RadarCFG {
     private static final List<Group> groups = new LinkedList<>();
 
     static {
-	String file = Config.loadFile("radar.json");
-	if(file != null) {
+	String xml = Config.loadFile("radar.xml");
+	if(xml != null) {
 	    try {
 		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
-		Document doc = builder.parse(file);
+		Document doc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
 		groups.clear();
 		NodeList groupNodes = doc.getElementsByTagName("group");
-		for(int i = 0; i < groupNodes.getLength(); i++) {
+		for (int i = 0; i < groupNodes.getLength(); i++) {
 		    groups.add(new Group((Element) groupNodes.item(i)));
 		}
-	    } catch(ParserConfigurationException | IOException | SAXException ignored) {
+	    } catch (ParserConfigurationException | IOException | SAXException ignored) {
+		ignored.printStackTrace();
 	    }
 	}
     }
 
+    public static void init(){
+
+    }
+
 
     public static class Group {
+	public String name;
+	public Boolean visible = null;
+	public Integer priority = null;
+	public List<MarkerCFG> markerCFGs;
 
 	public Group(Element config) {
-	    config.hasAttribute("name");
+	    name = config.getAttribute("name");
+	    if(config.hasAttribute("show")) {
+		visible = config.getAttribute("show").toLowerCase().equals("true");
+	    }
+	    if(config.hasAttribute("priority")) {
+		try {
+		    priority = Integer.parseInt(config.getAttribute("priority"));
+		} catch (NumberFormatException ignored) {
+		}
+	    }
+	    NodeList children = config.getElementsByTagName("match");
+	    markerCFGs = new LinkedList<>();
+	    for (int i = 0; i < children.getLength(); i++) {
+		markerCFGs.add(new MarkerCFG((Element) children.item(i), this));
+	    }
+
 	}
+    }
+
+    public static class MarkerCFG {
+	private final Group parent;
+	private Match type;
+	private String pattern;
+	private Boolean visible = null;
+	private Integer priority = null;
+	public String icon = "gfx/hud/mmap/o";
+	public String name = null;
+
+	public MarkerCFG(Element config, Group parent) {
+	    this.parent = parent;
+	    Match[] types = Match.values();
+	    String name = null;
+	    for (Match type : types) {
+		name = type.name();
+		if(config.hasAttribute(name)) {
+		    break;
+		}
+	    }
+	    if(name == null) {
+		throw new RuntimeException();
+	    }
+	    type = Match.valueOf(name);
+	    pattern = config.getAttribute(name);
+	    if(config.hasAttribute("show")) {
+		visible = config.getAttribute("show").toLowerCase().equals("true");
+	    }
+	    if(config.hasAttribute("image")) {
+		icon = config.getAttribute("image");
+	    }
+	    if(config.hasAttribute("priority")) {
+		try {
+		    priority = Integer.parseInt(config.getAttribute("priority"));
+		} catch (NumberFormatException ignored) {
+		}
+	    }
+	}
+
+	public boolean match(String target) {
+	    return type.match(pattern, target);
+	}
+
+	public boolean visible() {
+	    if(parent.visible != null && !parent.visible) {
+		return false;
+	    } else if(visible != null) {
+		return visible;
+	    } else {
+		return true;
+	    }
+	}
+
+	public int priority() {
+	    return (priority != null) ? priority : ((parent.priority != null) ? parent.priority : 0);
+	}
+    }
+
+    enum Match {
+	exact {
+	    @Override
+	    public boolean match(String pattern, String target) {
+		return target.equals(pattern);
+	    }
+	},
+	regex {
+	    @Override
+	    public boolean match(String pattern, String target) {
+		return target.matches(pattern);
+	    }
+	},
+	startsWith {
+	    @Override
+	    public boolean match(String pattern, String target) {
+		return target.startsWith(pattern);
+	    }
+	},
+	contains {
+	    @Override
+	    public boolean match(String pattern, String target) {
+		return target.contains(pattern);
+	    }
+	};
+
+	public abstract boolean match(String pattern, String target);
     }
 }
