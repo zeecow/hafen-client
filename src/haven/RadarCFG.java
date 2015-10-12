0002d66a80b387403c6adf50e65ffa4ba1ad274e
Ender Wiggin <andrii.gook@gmail.com>
2015-10-12 16:35:17 +0300

    basic saving of radar config

--- src/haven/RadarCFG.java
+++ src/haven/RadarCFG.java
@@ -8,36 +8,66 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
 
 public class RadarCFG {
     public static final List<Group> groups = new LinkedList<>();
+    private static DocumentBuilder builder;
 
     static {
 	String xml = Config.loadFile("radar.xml");
 	if(xml != null) {
 	    try {
 		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+		builder = documentBuilderFactory.newDocumentBuilder();
 		Document doc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
 		groups.clear();
 		NodeList groupNodes = doc.getElementsByTagName("group");
-		for(int i = 0; i < groupNodes.getLength(); i++) {
+		for (int i = 0; i < groupNodes.getLength(); i++) {
 		    groups.add(new Group((Element) groupNodes.item(i)));
 		}
-	    } catch(ParserConfigurationException | IOException | SAXException ignored) {
+	    } catch (ParserConfigurationException | IOException | SAXException ignored) {
 		ignored.printStackTrace();
 	    }
 	}
     }
 
-    public static void init() {
+    public static void save() {
+	try {
+	    Document doc = builder.newDocument();
 
+	    // construct XML
+	    Element root = doc.createElement("icons");
+	    doc.appendChild(root);
+	    for (Group group : groups) {
+		Element el = doc.createElement("group");
+		group.write(el);
+		root.appendChild(el);
+	    }
+
+	    // write XML
+	    OutputStream out = new ByteArrayOutputStream();
+	    Transformer transformer = TransformerFactory.newInstance().newTransformer();
+	    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+	    //transformer.setParameter();
+	    DOMSource source = new DOMSource(doc);
+	    StreamResult console = new StreamResult(out);
+	    transformer.transform(source, console);
+	    Config.saveFile("radar_saved.xml", out.toString());
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
     }
 
     private static Resource loadres(String name) {
@@ -46,27 +76,43 @@ public class RadarCFG {
 
     public static class Group {
 	public String name;
-	public Boolean visible = null;
+	public Boolean show = null;
 	public Integer priority = null;
 	public List<MarkerCFG> markerCFGs;
 
 	public Group(Element config) {
 	    name = config.getAttribute("name");
 	    if(config.hasAttribute("show")) {
-		visible = config.getAttribute("show").toLowerCase().equals("true");
+		show = config.getAttribute("show").toLowerCase().equals("true");
 	    }
 	    if(config.hasAttribute("priority")) {
 		try {
 		    priority = Integer.parseInt(config.getAttribute("priority"));
-		} catch(NumberFormatException ignored) {
+		} catch (NumberFormatException ignored) {
 		}
 	    }
 	    NodeList children = config.getElementsByTagName("match");
 	    markerCFGs = new LinkedList<>();
-	    for(int i = 0; i < children.getLength(); i++) {
+	    for (int i = 0; i < children.getLength(); i++) {
 		markerCFGs.add(MarkerCFG.parse((Element) children.item(i), this));
 	    }
 
+	}
+
+	public void write(Element el) {
+	    Document doc = el.getOwnerDocument();
+	    el.setAttribute("name", name);
+	    if(show != null) {
+		el.setAttribute("show", show.toString());
+	    }
+	    if(priority != null) {
+		el.setAttribute("priority", priority.toString());
+	    }
+	    for (MarkerCFG marker : markerCFGs) {
+		Element mel = doc.createElement("match");
+		marker.write(mel);
+		el.appendChild(mel);
+	    }
 	}
     }
 
@@ -74,7 +120,7 @@ public class RadarCFG {
 	private Group parent;
 	private Match type;
 	private String pattern;
-	private Boolean visible = null;
+	private Boolean show = null;
 	private Integer priority = null;
 	public String icon = null;
 	public String name = null;
@@ -86,7 +132,7 @@ public class RadarCFG {
 	    cfg.parent = parent;
 	    Match[] types = Match.values();
 	    String name = null;
-	    for(Match type : types) {
+	    for (Match type : types) {
 		name = type.name();
 		if(config.hasAttribute(name)) {
 		    break;
@@ -95,10 +141,11 @@ public class RadarCFG {
 	    if(name == null) {
 		throw new RuntimeException();
 	    }
+	    cfg.name = config.getAttribute("name");
 	    cfg.type = Match.valueOf(name);
 	    cfg.pattern = config.getAttribute(name);
 	    if(config.hasAttribute("show")) {
-		cfg.visible = config.getAttribute("show").toLowerCase().equals("true");
+		cfg.show = config.getAttribute("show").toLowerCase().equals("true");
 	    }
 	    if(config.hasAttribute("image")) {
 		cfg.icon = config.getAttribute("image");
@@ -106,7 +153,7 @@ public class RadarCFG {
 	    if(config.hasAttribute("priority")) {
 		try {
 		    cfg.priority = Integer.parseInt(config.getAttribute("priority"));
-		} catch(NumberFormatException ignored) {
+		} catch (NumberFormatException ignored) {
 		}
 	    }
 
@@ -128,7 +175,7 @@ public class RadarCFG {
 			this.tex = new TexI(buf);
 		    }
 
-		} catch(Loading ignored) {
+		} catch (Loading ignored) {
 		}
 	    }
 	    return tex;
@@ -140,10 +187,10 @@ public class RadarCFG {
 	}
 
 	public boolean visible() {
-	    if(parent.visible != null && !parent.visible) {
+	    if(parent.show != null && !parent.show) {
 		return false;
-	    } else if(visible != null) {
-		return visible;
+	    } else if(show != null) {
+		return show;
 	    } else {
 		return true;
 	    }
@@ -151,6 +198,22 @@ public class RadarCFG {
 
 	public int priority() {
 	    return (priority != null) ? priority : ((parent != null && parent.priority != null) ? parent.priority : 0);
+	}
+
+	public void write(Element el) {
+	    el.setAttribute(type.name(), pattern);
+	    if(icon != null) {
+		el.setAttribute("image", icon);
+	    }
+	    if(name != null) {
+		el.setAttribute("name", name);
+	    }
+	    if(show != null) {
+		el.setAttribute("show", show.toString());
+	    }
+	    if(priority != null) {
+		el.setAttribute("priority", priority.toString());
+	    }
 	}
     }
 
