bac92bb423b0b78fcc026b1360244d129c861f1e
EnderWiggin <andrii.gook@gmail.com>
2016-09-04 14:44:41 +0300

    Merge branch 'master' of https://github.com/dolda2000/hafen-client
    Conflicts:
    	src/haven/CharWnd.java
    some progress on key binds

--- src/haven/KeyBinder.java
+++ src/haven/KeyBinder.java
@@ -1,61 +1,79 @@
 package haven;
 
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class KeyBinder {
+    public static final int ALT = 1;
+    public static final int CTRL = 2;
+    public static final int SHIFT = 4;
+    private List<KeyBind> binds = new ArrayList<>();
+    
+    public KeyBinder() {
+	add(KeyEvent.VK_1, CTRL, gui -> gui.eqproxy.activate(0));
+	add(KeyEvent.VK_2, CTRL, gui -> gui.eqproxy.activate(1));
+	add(KeyEvent.VK_C, ALT, GameUI::toggleCraftList);
+	add(KeyEvent.VK_B, ALT, GameUI::toggleBuildList);
+	add(KeyEvent.VK_A, ALT, GameUI::toggleActList);
+	add(KeyEvent.VK_H, ALT, GameUI::toggleHand);
+	add(KeyEvent.VK_S, ALT, GameUI::toggleStudy);
+	add(KeyEvent.VK_F, ALT, gui -> gui.filter.toggle());
+	add(KeyEvent.VK_I, ALT, gui -> CFG.DISPLAY_GOB_INFO.set(!CFG.DISPLAY_GOB_INFO.get(), true));
+	add(KeyEvent.VK_H, CTRL, gui -> CFG.DISPLAY_GOB_HITBOX.set(!CFG.DISPLAY_GOB_HITBOX.get(), true));
+	add(KeyEvent.VK_R, ALT, gui -> CFG.SHOW_GOB_RADIUS.set(!CFG.SHOW_GOB_RADIUS.get(), true));
+	add(KeyEvent.VK_G, CTRL, gui -> gui.map.togglegrid());
+	add(KeyEvent.VK_Z, CTRL, gui ->
+		{
+		    Config.center_tile = !Config.center_tile;
+		    gui.ui.message(String.format("Tile centering turned %s", Config.center_tile ? "ON" : "OFF"), GameUI.MsgType.INFO);
+		}
+	);
+    }
+    
     public boolean handle(UI ui, KeyEvent e) {
 	int code = e.getKeyCode();
-	boolean CTRL = e.isControlDown();
-	boolean ALT = e.isAltDown();
-
-	if(CTRL && code == KeyEvent.VK_1) {
-	    if(ui.gui != null) {
-		ui.gui.eqproxy.activate(0);
+	int modflags = e.getModifiersEx();
+	modflags = ((modflags & InputEvent.ALT_DOWN_MASK) != 0 ? ALT : 0)
+		| ((modflags & InputEvent.META_DOWN_MASK) != 0 ? ALT : 0)
+		| ((modflags & InputEvent.CTRL_DOWN_MASK) != 0 ? CTRL : 0)
+		| ((modflags & InputEvent.SHIFT_DOWN_MASK) != 0 ? SHIFT : 0);
+	
+	for (KeyBind bind : binds) {
+	    if (bind.match(ui, code, modflags)) {
+		return true;
 	    }
-	} else if(CTRL && code == KeyEvent.VK_2) {
-	    if(ui.gui != null) {
-		ui.gui.eqproxy.activate(1);
-	    }
-	} else if(ALT && code == KeyEvent.VK_C) {
-	    if(ui.gui!=null){
-		ui.gui.toggleCraftList();
-	    }
-	} else if(ALT && code == KeyEvent.VK_B) {
-	    if(ui.gui!=null){
-		ui.gui.toggleBuildList();
-	    }
-	} else if(ALT && code == KeyEvent.VK_A) {
-	    if(ui.gui!=null){
-		ui.gui.toggleActList();
-	    }
-	} else if(ALT && code == KeyEvent.VK_H) {
-	    if(ui.gui!=null){
-		ui.gui.toggleHand();
-	    }
-	} else if(ALT && code == KeyEvent.VK_S) {
-	    if(ui.gui!=null){
-		ui.gui.toggleStudy();
-	    }
-	} else if(ALT && code == KeyEvent.VK_F) {
-	    if(ui.gui!=null){
-		FilterWnd filter = ui.gui.filter;
-		filter.show(!filter.visible);
-	    }
-	} else if(ALT && code == KeyEvent.VK_I) {
-	    CFG.DISPLAY_GOB_INFO.set(!CFG.DISPLAY_GOB_INFO.get(), true);
-	} else if(CTRL && code == KeyEvent.VK_H) {
-	    CFG.DISPLAY_GOB_HITBOX.set(!CFG.DISPLAY_GOB_HITBOX.get(), true);
-	} else if(ALT && code == KeyEvent.VK_R) {
-	    CFG.SHOW_GOB_RADIUS.set(!CFG.SHOW_GOB_RADIUS.get(), true);
-	} else if(CTRL && code == KeyEvent.VK_G) {
-	    if(ui.gui!=null){
-		ui.gui.map.togglegrid();
-	    }
-	} else if(CTRL && code == KeyEvent.VK_Z) {
-	    Config.center_tile = !Config.center_tile;
-	    ui.message(String.format("Tile centering turned %s", Config.center_tile ? "ON" : "OFF"), GameUI.MsgType.INFO);
 	}
-
+	
 	return false;
+    }
+    
+    public void add(int code, int mods, Action action) {
+	binds.add(new KeyBind(code, mods, action));
+    }
+    
+    public static class KeyBind {
+	private final int code;
+	private final int mods;
+	private final Action action;
+	
+	public KeyBind(int code, int mods, Action action) {
+	    this.code = code;
+	    this.mods = mods;
+	    this.action = action;
+	}
+	
+	public boolean match(UI ui, int code, int mods) {
+	    boolean match = code == this.code && ((mods & this.mods) == this.mods);
+	    if (match && ui.gui != null) {
+		action.run(ui.gui);
+	    }
+	    return match;
+	}
+    }
+    
+    public interface Action {
+	void run(GameUI gui);
     }
 }
