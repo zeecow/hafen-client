b018b28dfc1fcd937dcbd4169a08c8814a195600
Ender Wiggin <andrii.gook@gmail.com>
2019-02-12 11:24:59 +0200

    added ability to disable dragging for DraggableWidget

--- src/haven/DraggableWidget.java
+++ src/haven/DraggableWidget.java
@@ -6,9 +6,23 @@ public class DraggableWidget extends Widget {
     private UI.Grab dm;
     private Coord doff;
     private Window.WndCfg cfg;
+    private boolean draggable = true;
     
     public DraggableWidget(String name) {
 	this.name = name;
+    }
+    
+    public void draggable(boolean draggable) {
+	this.draggable = draggable;
+	if(!draggable) {stop_dragging();}
+    }
+    
+    private void stop_dragging() {
+	if(dm != null) {
+	    dm.remove();
+	    dm = null;
+	    updateCfg();
+	}
     }
     
     @Override
@@ -17,7 +31,7 @@ public class DraggableWidget extends Widget {
 	    parent.setfocus(this);
 	    return true;
 	}
-	if(c.isect(Coord.z, sz)) {
+	if(c.isect(Coord.z, sz) && draggable) {
 	    if(button == 1) {
 		dm = ui.grabmouse(this);
 		doff = c;
@@ -31,9 +45,7 @@ public class DraggableWidget extends Widget {
     @Override
     public boolean mouseup(Coord c, int button) {
 	if(dm != null) {
-	    dm.remove();
-	    dm = null;
-	    updateCfg();
+	    stop_dragging();
 	} else {
 	    super.mouseup(c, button);
 	}
