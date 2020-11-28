2288ee4671bf793e060d7dabfdd18033ac17ab89
Ender Wiggin <andrii.gook@gmail.com>
2020-11-28 10:04:37 +0200

    a bit of prettyfing the code

--- src/haven/L10N.java
+++ src/haven/L10N.java
@@ -5,8 +5,8 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -30,10 +30,10 @@ public class L10N {
 	INGREDIENT("ingredient"),
 	LABEL("label", true),
 	FLOWER("flower", true);
-
+	
 	public final String name;
 	public final boolean useMatch;
-
+	
 	Bundle(String name, boolean useMatch) {
 	    this.name = name;
 	    this.useMatch = useMatch;
@@ -63,19 +63,19 @@ public class L10N {
     public static String button(String text) {
 	return process(Bundle.BUTTON, text);
     }
-
+    
     public static String label(String text) {
 	return process(Bundle.LABEL, text);
     }
-
+    
     public static String flower(String text) {
 	return process(Bundle.FLOWER, text);
     }
-
+    
     public static String tooltip(String text) {
 	return process(Bundle.TOOLTIP, text);
     }
-
+    
     public static String tooltip(Resource res, String def) {
 	return process(Bundle.TOOLTIP, res.name, def);
     }
@@ -83,7 +83,7 @@ public class L10N {
     public static String ingredient(String text) {
 	return process(Bundle.INGREDIENT, text);
     }
-
+    
     private static String ingredient(Matcher m, int g) {
 	String value = m.group(g);
 	if(value.matches("[\\w\\s]+") && !value.matches("[\\d]+")) {
@@ -91,7 +91,7 @@ public class L10N {
 	}
 	return value;
     }
-
+    
     public static String pagina(Resource res, String def) {
 	return process(Bundle.PAGINA, res.name, def);
     }
@@ -99,11 +99,11 @@ public class L10N {
     public static String action(Resource res, String def) {
 	return process(Bundle.ACTION, res.name, def);
     }
-
+    
     private static String process(Bundle bundle, String key) {
 	return process(bundle, key, key);
     }
-
+    
     private static String process(Bundle bundle, String key, String def) {
 	String result = null;
 	if(key == null || key.isEmpty() || language == Language.EN) {
@@ -128,10 +128,10 @@ public class L10N {
 	    result = map.get(key);
 	}
 	if(DBG && result == null) {
-	    synchronized (MISSING){
-	    	String tmp = key.replaceAll("[()\\[\\]]", "\\\\$0");
-	    	MISSING.get(bundle).put(tmp, def);
-	    	Config.saveFile("MISSING_TRANSLATIONS.json", GSON_OUT.toJson(MISSING));
+	    synchronized (MISSING) {
+		String tmp = key.replaceAll("[()\\[\\]]", "\\\\$0");
+		MISSING.get(bundle).put(tmp, def);
+		Config.saveFile("MISSING_TRANSLATIONS.json", GSON_OUT.toJson(MISSING));
 	    }
 	}
 	return result != null ? result : def;
