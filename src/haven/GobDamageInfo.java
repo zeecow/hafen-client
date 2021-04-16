44b034bd5c8fc1f0468eeb16a8fb59017fb1d0a4
Ender Wiggin <andrii.gook@gmail.com>
2021-04-16 14:28:59 +0300

    reworked gob damage to show hhp and armor damage in addition to shp damage

--- src/haven/GobDamageInfo.java
+++ src/haven/GobDamageInfo.java
@@ -1,19 +1,30 @@
 package haven;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class GobDamageInfo extends GobInfo {
-    private static final Map<Long, Integer> gobDamage = new LinkedHashMap<Long, Integer>() {
+    private static final int SHP = 61455;
+    private static final int HHP = 64527;
+    private static final int ARM = 36751;
+    private static final int PAD = UI.scale(3);
+    
+    private static final Color BG = new Color(0, 0, 0, 128);
+    private static final Color SHP_C = Utils.col16(SHP);
+    private static final Color HHP_C = Utils.col16(HHP);
+    private static final Color ARM_C = Utils.col16(ARM);
+    
+    private static final Map<Long, DamageVO> gobDamage = new LinkedHashMap<Long, DamageVO>() {
 	@Override
 	protected boolean removeEldestEntry(Map.Entry eldest) {
 	    return size() > 50;
 	}
     };
     
-    private int damage = 0;
+    private final DamageVO damage;
     
     public GobDamageInfo(Gob owner) {
 	super(owner);
@@ -21,6 +32,9 @@ public class GobDamageInfo extends GobInfo {
 	center = new Pair<>(0.5, 1.0);
 	if(gobDamage.containsKey(gob.id)) {
 	    damage = gobDamage.get(gob.id);
+	} else {
+	    damage = new DamageVO();
+	    gobDamage.put(gob.id, damage);
 	}
     }
     
@@ -31,22 +45,39 @@ public class GobDamageInfo extends GobInfo {
     
     @Override
     protected Tex render() {
-	if(damage > 0) {
-	    return Text.std.renderstroked(String.format("%d", damage), Color.RED, Color.BLACK).tex();
+	if(damage.isEmpty()) {return null;}
+	
+	BufferedImage hhp = null, shp = null, arm = null;
+	if(damage.shp > 0) {
+	    hhp = Text.std.render(String.format("%d", damage.shp), SHP_C).img;
 	}
-	return null;
+	if(damage.hhp > 0) {
+	    shp = Text.std.render(String.format("%d", damage.hhp), HHP_C).img;
+	}
+	if(damage.armor > 0) {
+	    arm = Text.std.render(String.format("%d", damage.armor), ARM_C).img;
+	}
+	return new TexI(ItemInfo.catimgsh(PAD, PAD, BG, hhp, shp, arm));
     }
     
     public void update(int c, int v) {
-	//System.out.println(String.format("Number %d, c: %d", v, c));
-	//64527 - SHP
-	//61455 - HHP
+//	Debug.log.println(String.format("Number %d, c: %d", v, c));
 	//35071 - Initiative
-	if(c == 61455) {//health
-	    damage += v;
-	    gobDamage.put(gob.id, damage);
-	    clean();
+	if(c == SHP) {
+	    damage.shp += v;
+	    update();
+	} else if(c == HHP) {
+	    damage.hhp += v;
+	    update();
+	} else if(c == ARM) {
+	    damage.armor += v;
+	    update();
 	}
+    }
+    
+    private void update() {
+	gobDamage.put(gob.id, damage);
+	clean();
     }
     
     public static boolean has(Gob gob) {
@@ -69,5 +100,12 @@ public class GobDamageInfo extends GobInfo {
 	for (long id : gobIds) {
 	    clearDamage(gui.ui.sess.glob.oc.getgob(id), id);
 	}
+    }
+    
+    private static class DamageVO {
+	int shp = 0, hhp = 0, armor = 0;
+	
+	boolean isEmpty() {return shp == 0 && hhp == 0 && armor == 0;}
+	
     }
 }
