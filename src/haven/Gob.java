fd3c024dad65da2bbd2bcc8f8ec3cf763eaa2d9e
Ender Wiggin <andrii.gook@gmail.com>
2021-04-26 19:36:14 +0300

    manually add/remove drawable from slots instead of setting attribute to avoid deadlocks

--- src/haven/Gob.java
+++ src/haven/Gob.java
@@ -972,10 +972,15 @@ public class Gob implements RenderTree.Node, Sprite.Owner, Skeleton.ModOwner, Sk
 	    Boolean needHide = CFG.HIDE_TREES.get();
 	    if(d != null && d.skipRender != needHide) {
 		d.skipRender = needHide;
-		glob.loader.defer(() -> setattr(d), null);
 		if(needHide) {
 		    tag(GobTag.HIDDEN);
+		    if(d.slots != null) {
+			ArrayList<RenderTree.Slot> tmpSlots = new ArrayList<>(d.slots);
+			glob.loader.defer(() -> RUtils.multirem(tmpSlots), null);
+		    }
 		} else {
+		    ArrayList<RenderTree.Slot> tmpSlots = new ArrayList<>(slots);
+		    glob.loader.defer(() -> RUtils.multiadd(tmpSlots, d), null);
 		    untag(GobTag.HIDDEN);
 		}
 	    	return true;
