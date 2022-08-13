d363dab3ac391d0a7e7e44c2b0f8578150ae7a61
zeecow <zeecow@github>
2022-08-13 18:46:58 -0300

    minitrees exclude bushes

--- src/haven/res/lib/tree/Tree.java
+++ src/haven/res/lib/tree/Tree.java
@@ -104,7 +104,7 @@ public class Tree extends Sprite {
 		if (ZeeConfig.treeAnimation)
 	    	gob.setattr(new GobSvaj(gob));
 
-		if (ZeeConfig.miniTrees)
+		if (ZeeConfig.miniTrees && !res.name.contains("/bushes/"))
 			gob.setattr(new TreeScale(gob, ZeeConfig.miniTreesSize / 100f));
 		else if(fscale != 1.0f)
 			gob.setattr(new TreeScale(gob, fscale));
