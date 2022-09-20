c3d2f6e0b5031adbb3ee8daa65df87655cbc9a2f
zeecow <zeecow@github>
2022-09-20 19:18:59 -0300

    shapeicons

--- src/haven/ZeeConfig.java
+++ src/haven/ZeeConfig.java
@@ -93,7 +93,7 @@ public class ZeeConfig {
     public static final String DEF_LIST_CONFIRM_PETAL = "Empty,Swill,Clean out,Take possession,Renounce Lawspeaker,Become Lawspeaker";
     public static final String DEF_LIST_BUTCH_AUTO = "Break,Scale,Wring neck,Kill,Skin,Flay,Pluck,Clean,Butcher,Collect bones";
     public static final String DEF_LIST_AUTO_CLICK_MENU = "Pick,Harvest wax";
-    public static final String DEF_LIST_SHAPEICON = "/knarr 2,boat 10 0 0,255 255 102;/rowboat 2,boat 5 0 0,255 255 102;/dugout 2,boat 5 0 0,255 255 102;/horse/ 1,square 4 0 1,0 255 255;stalagoomba 1,diamond 10 1 0,255 255 0;/snekkja 2,boat 8 0 1,255 255 102;/cart 2,square 4 0 1,204 204 204;/wheelbarrow 2,square 4 0 1,204 204 255;/amberwash 2,diamond 10 0 1,255 102 0;/cavepuddle 2,diamond 7 0 1,0 204 102;/ladder 2,triangleUp 6 0 1,0 204 102;/minehole 2,triangleDown 6 0 1,0 204 102;/burrow 2,triangleDown 6 0 1,204 0 255";
+    public static final String DEF_LIST_SHAPEICON = "stalagoomba 1,diamond 7 1 0,255 255 0;/amberwash 2,diamond 7 0 1,255 102 0;/cavepuddle 2,diamond 7 0 1,0 204 102;/ladder 2,triangleUp 5 0 1,0 204 102;/minehole 2,triangleDown 5 0 1,0 204 102;/burrow 2,triangleDown 6 0 1,204 0 255;/spark 2,square 4 0 1,102 102 255;/snekkja 2,square 4 0 1,255 255 102;/dugout 2,square 4 0 1,255 255 102;/wheelbarrow 2,square 4 0 1,0 255 255;/cart 2,square 4 0 1,0 153 255;/knarr 2,square 4 0 1,255 255 102;/rowboat 2,square 4 0 1,255 255 102;/horse/ 1,square 4 0 1,0 204 0";
     public static final Color DEF_SIMPLE_WINDOW_COLOR = new Color(55, 64, 32, 255);
     public static final Color DEF_GRID_COLOR = new Color(204, 204, 255, 45);
 
