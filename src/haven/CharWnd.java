42734ebba0efcbc0489666b2c9489c23a7a34101
zeecow <zeecow@github>
2022-11-05 19:51:03 -0300

    attr font serif16

--- src/haven/CharWnd.java
+++ src/haven/CharWnd.java
@@ -26,13 +26,16 @@
 
 package haven;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.util.*;
-import java.util.function.*;
-import haven.resutil.FoodInfo;
 import haven.resutil.Curiosity;
+import haven.resutil.FoodInfo;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 import static haven.PUtils.*;
 
 /* XXX: There starts to seem to be reason to split the while character
@@ -42,7 +45,7 @@ public class CharWnd extends Window {
     public static final RichText.Foundry ifnd = new RichText.Foundry(Resource.remote(), java.awt.font.TextAttribute.FAMILY, "SansSerif", java.awt.font.TextAttribute.SIZE, UI.scale(9)).aa(true);
     public static final Text.Furnace catf = new BlurFurn(new TexFurn(new Text.Foundry(Text.fraktur, 25).aa(true), Window.ctex), UI.scale(3), UI.scale(2), new Color(96, 48, 0));
     public static final Text.Furnace failf = new BlurFurn(new TexFurn(new Text.Foundry(Text.fraktur, 25).aa(true), Resource.loadimg("gfx/hud/fontred")), UI.scale(3), UI.scale(2), new Color(96, 48, 0));
-    public static final Text.Foundry attrf = new Text.Foundry(Text.fraktur, 18).aa(true);
+    public static final Text.Foundry attrf =  new Text.Foundry(Text.serif, 16).aa(true);//new Text.Foundry(Text.fraktur, 18).aa(true);
     public static final Color debuff = new Color(255, 128, 128);
     public static final Color buff = new Color(128, 255, 128);
     public static final Color tbuff = new Color(128, 128, 255);
