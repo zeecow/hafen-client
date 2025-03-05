d5d2922a51320b3eb33aec1c6c8c4964bf20bd1f
zeecow <zeecow@github>
2025-03-05 10:39:16 -0300

    rescode

--- src/haven/res/ui/tt/attrmod/AttrMod.java
+++ src/haven/res/ui/tt/attrmod/AttrMod.java
@@ -9,53 +9,65 @@ import java.util.Collection;
 
 import static haven.PUtils.convolvedown;
 
-/* >tt: AttrMod$Fac */
-@haven.FromResource(name = "ui/tt/attrmod", version = 10)
+@Resource.PublishedCode(name = "attrmod")
+@haven.FromResource(name = "ui/tt/attrmod", version = 11)
 public class AttrMod extends ItemInfo.Tip {
-    public final Collection<Mod> mods;
+    public final Collection<Entry> tab;
 
-    public static class Mod {
-	public final Resource attr;
-	public final int mod;
-
-	public Mod(Resource attr, int mod) {this.attr = attr; this.mod = mod;}
-    }
-
-    public AttrMod(Owner owner, Collection<Mod> mods) {
+    public AttrMod(Owner owner, Collection<Entry> tab) {
 	super(owner);
-	this.mods = mods;
+	this.tab = tab;
     }
 
     public static class Fac implements InfoFactory {
 	public ItemInfo build(Owner owner, Raw raw, Object... args) {
 	    Resource.Resolver rr = owner.context(Resource.Resolver.class);
-	    Collection<Mod> mods = new ArrayList<Mod>();
+	    Collection<Entry> tab = new ArrayList<Entry>();
 	    for(int a = 1; a < args.length; a += 2)
-		mods.add(new Mod(rr.getres((Integer)args[a]).get(), (Integer)args[a + 1]));
-	    return(new AttrMod(owner, mods));
-	}
-
-	public ItemInfo build(Owner owner, Object... args) {
-	    return(null);
+		tab.add(new Mod(Attribute.get(rr.getresv(args[a]).get()), Utils.dv(args[a + 1])));
+	    return(new AttrMod(owner, tab));
 	}
     }
 
-    private static String buff = "128,255,128", debuff = "255,128,128";
-    public static BufferedImage modimg(Collection<Mod> mods) {
-	Collection<BufferedImage> lines = new ArrayList<BufferedImage>(mods.size());
-	for(Mod mod : mods) {
-	    BufferedImage line = RichText.render(String.format("%s $col[%s]{%s%d}", mod.attr.layer(Resource.tooltip).t,
-							       (mod.mod < 0)?debuff:buff, (mod.mod < 0)?'-':'+', Math.abs(mod.mod)),
-						 0).img;
-	    BufferedImage icon = convolvedown(mod.attr.layer(Resource.imgc).img,
-					      new Coord(line.getHeight(), line.getHeight()),
-					      CharWnd.iconfilter);
-	    lines.add(catimgsh(0, icon, line));
+    public static void tablayout(Layout l, Collection<Entry> tabc) {
+	Entry[] tab = tabc.toArray(new Entry[0]);
+	BufferedImage[] icons = new BufferedImage[tab.length];
+	BufferedImage[] names = new BufferedImage[tab.length];
+	BufferedImage[] values = new BufferedImage[tab.length];
+	int w = 0;
+	for(int i = 0; i < tab.length; i++) {
+	    Entry row = tab[i];
+	    names[i] = Text.render(row.attr.name()).img;
+	    icons[i] = row.attr.icon();
+	    if(icons[i] != null)
+		icons[i] = convolvedown(icons[i], Coord.of(names[i].getHeight()), CharWnd.iconfilter);
+	    values[i] = RichText.render(row.fmtvalue(), 0).img;
+	    w = Math.max(w, names[i].getWidth());
 	}
-	return(catimgs(0, lines.toArray(new BufferedImage[0])));
+	for(int i = 0; i < tab.length; i++) {
+	    int y = l.cmp.sz.y;
+	    if(icons[i] != null)
+		l.cmp.add(icons[i], Coord.of(0, y));
+	    int nx = names[i].getHeight() + (int)UI.scale(0.75);
+	    l.cmp.add(names[i], Coord.of(nx, y));
+	    l.cmp.add(values[i], Coord.of(nx + w + UI.scale(5), y));
+	}
     }
 
-    public BufferedImage tipimg() {
-	return(modimg(mods));
+    public void prepare(Layout l) {
+	l.intern(Collected.id).tab.addAll(tab);
+    }
+
+    public static class Collected extends Tip {
+	public static final Layout.TipID<Collected> id = Collected::new;
+	public final Collection<Entry> tab = new ArrayList<>();
+
+	public Collected(Owner owner) {
+	    super(owner);
+	}
+
+	public void layout(Layout l) {
+	    tablayout(l, tab);
+	}
     }
 }
